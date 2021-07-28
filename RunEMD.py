40caafd58d78371cd0b85da08991ddc55e65d685
chathika <chathikagunaratne@gmail.com>
2021-07-28 02:57:15 +0000

    EMD code updated with new parameters

--- RunEMD.py
+++ RunEMD.py
@@ -1,25 +1,24 @@
 from EvolutionaryModelDiscovery import *
 import numpy as np
-modelPath = "SimpleSchellingTwoSubgroups_HatnaAdaption.nlogo"
+modelPath = "SimpleSchellingTwoSubgroups_Hatna_EMD.nlogo"
 setup = [
     'set neighborhood-distance 1',
-    'set empty-cells-to-evaluate-frac 1',
     'set prob-of-relocation-attempt-by-happy random 0.01',
     'set fraction-of-blue 0.5',
-    'set density 0.5 + (0.05 * random 10)',
-    'set tolerance-dist-blue "0.125,0.5\\n0.833,0.5"',
-    'set tolerance-dist-green "0.125,0.5\\n0.833,0.5"',
+    'set density 0.5 + (0.05 * random 4)',
+    'set tolerance-dist-A 0.5 + (0.05 * random 4)',
+    'set history-length 10',
     'setup']
 measurements = ["c-index"]
 ticks = 10
 emd = EvolutionaryModelDiscovery("/opt/netlogo/", modelPath,setup, measurements, ticks)
 emd.setMutationRate(0.1)
 emd.setCrossoverRate(0.8)
-emd.setGenerations(50)
+emd.setGenerations(20)
 emd.setReplications(5)
 emd.setDepth(2,6)
-emd.setPopulationSize(5)
-#emd.setIsMinimize(False)
+emd.setPopulationSize(20)
+emd.setIsMinimize(False)
 
 def cindexObjective(results):
     #print(results.iloc[-1][0])
@@ -28,7 +27,5 @@ def cindexObjective(results):
 emd.setObjectiveFunction(cindexObjective)
 
 if __name__ == '__main__':
-    import warnings
-    warnings.filterwarnings("ignore", category=DeprecationWarning) 
     emd.evolve()
     
