a15ab206e25f441d87a4beb2040710d1b425d4cb
chathika <chathikagunaratne@gmail.com>
2021-10-25 13:23:04 +0000

    Run script changes

--- RunEMD.py
+++ RunEMD.py
@@ -1,30 +1,40 @@
 from EvolutionaryModelDiscovery import *
 import numpy as np
+import random
 modelPath = "SimpleSchellingTwoSubgroups_Hatna.nlogo"
 setup = [
     'set neighborhood-distance 1',
-    'set prob-of-relocation-attempt-by-happy random 0.01',
+    'set prob-of-relocation-attempt-by-happy 0.01',
     'set fraction-of-blue 0.5',
-    'set density 0.5 + (0.05 * random 4)',
-    'set tolerance-dist-A 0.5 + (0.05 * random 4)',
+    'set density 0.95',
+    'set threshold-group-A 0.4',
     'set history-length 10',
     'setup']
+
+all_setup_commands = []
+for j in range(5):
+    i = random.uniform(0.3,0.8)
+    s = setup.copy()
+    s[4] = f'set threshold-group-A {i}'
+    all_setup_commands.append(s)
+
+
+
 measurements = ["c-index"]
-ticks = 10
-emd = EvolutionaryModelDiscovery("/home/social-sim/NetLogo 6.2.0/", modelPath,setup, measurements, ticks)
-emd.setMutationRate(0.1)
-emd.setCrossoverRate(0.8)
-emd.setGenerations(20)
-emd.setReplications(5)
-emd.setDepth(2,6)
-emd.setPopulationSize(20)
-emd.setIsMinimize(False)
+ticks = 500
+emd = EvolutionaryModelDiscovery("/home/social-sim/NetLogo 6.2.0/", modelPath,all_setup_commands, measurements, ticks, agg_func = np.mean)
+emd.set_mutation_rate(0.2)
+emd.set_crossover_rate(0.8)
+emd.set_generations(20)
+emd.set_replications(1)
+emd.set_depth(4,20)
+emd.set_population_size(20)
+emd.set_is_minimize(False)
 
-def cindexObjective(results):
-    #print(results.iloc[-1][0])
-    return np.mean(results.iloc[-1])
+def c_index_objective(results):
+    return np.mean(results.iloc[-100:,0])
 
-emd.setObjectiveFunction(cindexObjective)
+emd.set_objective_function(c_index_objective)
 
 if __name__ == '__main__':
     emd.evolve()
